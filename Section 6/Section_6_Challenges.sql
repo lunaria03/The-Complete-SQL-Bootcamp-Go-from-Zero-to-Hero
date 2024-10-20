6020070e8d114062db364086dc11feb0c354a402
Oriza Sativa <70560591+lunaria03@users.noreply.github.com>
2024-10-20 13:33:49 +0700

    Update Section_6_Challenges.sql

--- Section 6/Section_6_Challenges.sql
+++ Section 6/Section_6_Challenges.sql
@@ -1,2 +1,9 @@
-/* Assessment Test 1 */
+/* Section 6: Advanced SQL Commands */
+/* Challenge #1: During which months did payments occur? 
+   Format your answer to return back the full month name. */
+SELECT DISTINCT(TO_CHAR(payment_date,'MONTH')) AS month FROM payment;
 
+/* Challenge #2: How many payments occured on a Monday? */
+SELECT COUNT(payment_id) 
+FROM payment
+WHERE (TO_CHAR(payment_date,'FMDAY')) = 'MONDAY';
